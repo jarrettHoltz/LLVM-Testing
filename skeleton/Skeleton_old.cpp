20b637c79aa5aaf5aff6fe2cc054edb38bd897b2
jarrettHoltz <random.jmh@gmail.com>
2017-03-27 17:57:18 -0400

    Adding old skele

---
+++ skeleton/Skeleton_old.cpp
@@ -0,0 +1,145 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/ValueSymbolTable.h"
+#include "llvm/IR/ValueMap.h"
+#include <iostream>
+using namespace llvm;
+
+namespace {
+  struct SkeletonPass : public FunctionPass {
+    static char ID;
+    SkeletonPass() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F) {
+      // Get the function to call from our runtime library.
+      LLVMContext &Ctx = F.getContext();
+      Constant *logFunc = F.getParent()->getOrInsertFunction(
+        "logop", Type::getVoidTy(Ctx), Type::getInt32Ty(Ctx), NULL
+      );
+      Constant *initialFunc = F.getParent()->getOrInsertFunction(
+        "LogInitial", Type::getVoidTy(Ctx), Type::getInt32Ty(Ctx),  NULL
+      );
+      Constant *assignedFunc = F.getParent()->getOrInsertFunction(
+        "LogAssigned", Type::getVoidTy(Ctx), Type::getInt32Ty(Ctx), NULL
+      );
+      ValueSymbolTable* prev_table = new ValueSymbolTable();
+      int x = 0;
+      for (auto &B : F) {
+
+        for (auto &I : B) {
+
+          if (I.getOpcode() == Instruction::Store) {
+            std::cout<< std::endl << std::endl;
+            // Insert at the point where the instruction `op` appears.
+            llvm::StoreInst *op = dyn_cast<llvm::StoreInst>(&I);
+            op->dump();
+            IRBuilder<> builder(op);
+            IRBuilder<> builder2(op);
+            IRBuilder<> builder3(op);
+            builder.SetInsertPoint(&B, --builder.GetInsertPoint());
+            builder.SetInsertPoint(&B, --builder.GetInsertPoint());
+            builder3.SetInsertPoint(&B, ++builder3.GetInsertPoint());
+            // Insert a call to our function.
+            Value *value = op->getValueOperand();
+            Value *pointer = op->getPointerOperand();
+            std::cout<<"test"<<std::endl;
+
+            for(auto U : pointer->users()){  // U is of type User*
+              if (auto j= dyn_cast<Instruction>(U)){
+                j->dump();
+                if (j->getOpcode() == Instruction::Load) {
+                  llvm::LoadInst *inner = dyn_cast<llvm::LoadInst>(j);
+//                      j->dump();
+
+//                   Value *value2 = inner->getValueOperand();
+                  Value *pointer2 = inner->getPointerOperand();
+                  std::cout << "inner" << std::endl;
+                  for(auto u : j->users()){  // U is of type User*
+                    if (auto J= dyn_cast<Instruction>(u)){
+                      J->dump();
+                    }
+                  }
+
+              }
+            }
+            }
+//             symbolTable->dump();
+//             ValueMap::iterator = symbolTable->begin();
+             ValueSymbolTable* symbolTable = B.getValueSymbolTable();
+            if(pointer->hasName() && pointer->getName().str() == "num") {
+             Value* start_name = builder.CreateGlobalString(pointer->getName());
+
+             Value* args[] = {start_name, pointer};
+//              builder.CreateCall(initialFunc, args);
+             std::vector<Value*> arguements;
+            for(StringMapIterator<Value *> iter=symbolTable->begin(); iter !=
+                symbolTable->end(); iter++) {
+//               iter->first->dump();
+              Value* iter_value = iter->second;
+              if(iter_value->hasName()) {
+//                   iter_value->dump();
+                  if(!prev_table->empty()) {
+                  Value* last_value = prev_table->lookup(iter_value->getName());
+                  if(iter_value->getName().str() != "argc" &&
+                     iter_value->getName().str() != "argv"
+                  ) {
+                  Value* name =
+                      builder.CreateGlobalString(iter_value->getName());
+                      arguements.push_back(name);
+//                   builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+//                   builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+//                   builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+                      arguements.push_back(iter_value);
+//                    Value* args2[] = {name, iter_value};
+//                    builder3.CreateCall(assignedFunc, args2);
+//                   builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+                  }
+                  }
+              }
+            }
+
+              Value* size = ConstantInt::get(Type::getInt32Ty(Ctx), arguements.size()/2);
+              arguements.insert(arguements.begin(),size);
+              ArrayRef<Value*>*  args2 = new ArrayRef<Value*>(arguements);
+              if(arguements.size() > 0) {
+//                 Value* args2[] = {size, arguements[0], arguements[1]};
+              CallInst* inst = builder3.CreateCall(assignedFunc, *args2);
+              CallInst* inst2 = builder.CreateCall(initialFunc, *args2);
+//               inst->dump();
+              }
+              prev_table = symbolTable;
+             }
+
+
+
+//             builder2.CreateCall(logFunc, rhs);
+//             return true;
+          }
+//            std::cout << "loop" << std::endl;
+        }
+
+      }
+
+      return false;
+    }
+  };
+}
+
+char SkeletonPass::ID = 0;
+
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
+static void registerSkeletonPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM) {
+  PM.add(new SkeletonPass());
+}
+static RegisterStandardPasses
+  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                 registerSkeletonPass);
